7d3b3cf8bc2e05e6f94e14d1a5c3a497a8cd402a
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-08-29 09:14:04 -0600

    Initial commit

--- src/App/App.js
+++ src/App/App.js
@@ -5,15 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <h1 className='app-title'>My Order History</h1>
-          <div className='purchase-form'>
-
-          </div>
-        </header>
-        <div className='purchase-container'>
-
-        </div>
+          <h2>hi</h2>
       </div>
     );
   }
